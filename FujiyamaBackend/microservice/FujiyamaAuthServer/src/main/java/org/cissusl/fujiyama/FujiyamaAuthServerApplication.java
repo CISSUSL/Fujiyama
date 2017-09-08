8e9f0814e6dea447466a057ee8e91199261b72de
Wthamira <thamira1005@gmail.com>
2017-09-08 15:41:45 +0530

    enviroment setup

---
+++ FujiyamaBackend/microservice/FujiyamaAuthServer/src/main/java/org/cissusl/fujiyama/FujiyamaAuthServerApplication.java
@@ -0,0 +1,12 @@
+package org.cissusl.fujiyama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujiyamaAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujiyamaAuthServerApplication.class, args);
+	}
+}
